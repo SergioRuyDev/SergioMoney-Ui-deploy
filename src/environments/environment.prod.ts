12b119773a41fdb3c9b4df0c39fc5b377815d733
sergioruydev <sergioruyenator@gmail.com>
2023-02-23 15:40:47 -0300

    Deploy v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algamoney-api-deploy-production.up.railway.app/',
+  apiUrl: 'https://algamoney-api-deploy-production.up.railway.app',
   tokenAllowedDomains: [ /algamoney-api-deploy-production.up.railway.app/ ],
   tokenDisallowedRoutes: [/\/oauth\/token/],
 
